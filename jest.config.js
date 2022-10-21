1d01f35a81b96e49fcabc97bdbc2e852b1de3883
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-10-21 13:27:48 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
