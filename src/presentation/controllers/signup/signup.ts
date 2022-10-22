e12bf7079678187756e0f905b4e7c28df43035b8
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-10-22 16:52:27 -0300

    chore: add jest configs and scripts

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -33,6 +33,7 @@ export class SignUpController implements Controller {
       })
       return ok(account)
     } catch (error) {
+      console.error(error)
       return serverRequest()
     }
   }
