91c9c25ccbe70208a9ad4c2bbab9c97063da54f2
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-10-26 15:27:06 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -33,7 +33,6 @@ export class SignUpController implements Controller {
       })
       return ok(account)
     } catch (error) {
-      console.error(error)
       return serverRequest()
     }
   }
