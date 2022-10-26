b091aab30e980cc628e0d360ef863761c3263a8c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-10-26 16:45:22 -0300

    feat: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { badRequest, serverRequest, ok } from '../../helpers/http-helper'
+import { badRequest, serverError, ok } from '../../helpers/http-helper'
 import { Controller, EmailValidator, HttpResponse, HttpRequest, AddAccount } from './signup-protocols'
 
 export class SignUpController implements Controller {
@@ -33,7 +33,7 @@ export class SignUpController implements Controller {
       })
       return ok(account)
     } catch (error) {
-      return serverRequest(error)
+      return serverError(error)
     }
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -4,7 +4,7 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
-export const serverRequest = (error: Error): HttpResponse => ({
+export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
   body: new ServerError(error.stack)
 })
