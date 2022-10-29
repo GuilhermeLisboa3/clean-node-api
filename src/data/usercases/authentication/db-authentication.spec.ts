af1e754b0a3675bbc5dac0b28bac1d86e486b60e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-10-29 14:27:25 -0300

    test: ensure returns null if LoadAccountByEmailRepository returns null

--- src/data/usercases/authentication/db-authentication.spec.ts
+++ src/data/usercases/authentication/db-authentication.spec.ts
@@ -49,4 +49,10 @@ describe('DbAuthetication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+  it('should return null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBeNull()
+  })
 })
