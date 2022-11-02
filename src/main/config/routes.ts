42d7198e24d56c8a2a7395cca7292beac629f371
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-11-02 17:06:21 -0300

    chore: enable degub with vscode

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,10 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
-import { join } from 'path'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  readdirSync(join(__dirname, '../routes')).map(async file => {
-    if (!file.includes('.test.')) {
+  readdirSync(`${String(__dirname)}/../routes`).map(async (file) => {
+    if (!file.endsWith('.map') && !file.includes('.test.')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
