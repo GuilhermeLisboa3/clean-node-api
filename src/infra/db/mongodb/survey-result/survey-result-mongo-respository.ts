198de16fa67801737489f53f216f95e09c3af451
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-11-10 15:36:26 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-respository.ts
@@ -0,0 +1,23 @@
+import { MongoHelper } from './../helpers/mongo-helper'
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { SaveSurveyResultModel } from '@/domain/usercases/save-survey-result'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    const res = await surveyResultCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        data: data.date
+      }
+    }, {
+      upsert: true,
+      returnDocument: 'after'
+    })
+    return MongoHelper.map(res.value)
+  }
+}
