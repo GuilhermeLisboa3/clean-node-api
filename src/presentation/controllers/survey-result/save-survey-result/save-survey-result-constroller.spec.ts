6377d6514bfdda4aed4fabc67c1a15b1fffe36dc
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-11-11 13:35:04 -0300

    test: ensure SaveSurveyResultController returns 500 if SaveSurveyResult throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-constroller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-constroller.spec.ts
@@ -120,4 +120,11 @@ describe('SaveSurveyResult Controller', () => {
       answer: 'any_answer'
     })
   })
+
+  it('should return 500 if SaveSurveyResult throws', async () => {
+    const { sut, saveSurveyResultSutb } = makeSut()
+    jest.spyOn(saveSurveyResultSutb, 'save').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
