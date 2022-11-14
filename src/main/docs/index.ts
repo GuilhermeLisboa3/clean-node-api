bac39032aff241fa82905774d101b8826cb72db3
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-11-14 12:19:53 -0300

    docs: add add-signup api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,5 +1,5 @@
 import { loginPath, surveyPath, singUpPath } from './path'
-import { accountSchema, errorSchema, loginParamsSchema, surveyAnswerSchema, surveySchema, surveysSchema, apiKeyAuthSchema, signUpParamsSchema } from './schemas'
+import { accountSchema, errorSchema, loginParamsSchema, surveyAnswerSchema, surveySchema, surveysSchema, apiKeyAuthSchema, signUpParamsSchema, addSurveyParamsSchema } from './schemas'
 import { badRequest, serverError, unauthorized, forbidden, notFound } from './components'
 export default {
   openapi: '3.0.0',
@@ -23,6 +23,7 @@ export default {
     account: accountSchema,
     loginParams: loginParamsSchema,
     signUpParams: signUpParamsSchema,
+    addSurveyParams: addSurveyParamsSchema,
     error: errorSchema,
     surveys: surveysSchema,
     survey: surveySchema,
--- src/main/docs/path/survey-path.ts
+++ src/main/docs/path/survey-path.ts
@@ -27,5 +27,39 @@ export const surveyPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+
+  post: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'Api para criar uma enquete',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/addSurveyParams'
+          }
+        }
+      }
+    },
+    responses: {
+      204: {
+        description: 'Sucesso'
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }
