936c3315f21912d74e2fd8d7befa9f44f50d0862
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-11-14 17:33:13 -0300

    docs: update save survey result docs

---
+++ src/main/docs/schemas/survey-result-answer-schema.ts
@@ -0,0 +1,19 @@
+
+export const surveyResultAnswerSchema = {
+  type: 'object',
+  properties: {
+    image: {
+      type: 'string'
+    },
+    answer: {
+      type: 'string'
+    },
+    count: {
+      type: 'number'
+    },
+    percent: {
+      type: 'number'
+    }
+  },
+  required: ['answer', 'count', 'percent']
+}
