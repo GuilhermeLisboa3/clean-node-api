1fb4c2902b9c33e614ac42697dc60a42382a54a1
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-11-17 15:31:09 -0300

    docs: add load survey result path

--- src/main/docs/path/survey-result-path.ts
+++ src/main/docs/path/survey-result-path.ts
@@ -44,5 +44,41 @@ export const surveyResultPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+  get: {
+    tags: ['Enquete'],
+    summary: 'Api para consultar o resultado de uma enquete',
+    security: [{
+      apiKeyAuth: []
+    }],
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }
