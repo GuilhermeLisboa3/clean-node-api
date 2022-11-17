1258f36c675bf5f03d0f2dcadfb2a33fc584a457
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-11-17 13:03:52 -0300

    feat: ensure DbLoadSurveyResult returns empty survey result if there is no result

--- src/domain/test/mock-survey.ts
+++ src/domain/test/mock-survey.ts
@@ -6,8 +6,9 @@ export const mockSurveyModel = (): SurveyModel => {
     id: 'any_id',
     question: 'any_question',
     answers: [{
-      image: 'any_image',
       answer: 'any_answer'
+    }, {
+      answer: 'other_answer'
     }],
     date: new Date()
   }
