cf0b7fe31f479668d4954d2de1331ea5d4ed6d45
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-11-18 12:11:07 -0300

    feat: add name to authentication response

--- src/data/usercases/account/authentication/db-authentication.ts
+++ src/data/usercases/account/authentication/db-authentication.ts
@@ -4,7 +4,8 @@ import {
   LoadAccountByEmailRepository,
   HashComparer,
   Encrypter,
-  UpdatedAccessTokenRepository
+  UpdatedAccessTokenRepository,
+  AuthenticationModel
 } from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
@@ -15,14 +16,17 @@ export class DbAuthentication implements Authentication {
     private readonly updatedAccessTokenRepository: UpdatedAccessTokenRepository
   ) {}
 
-  async auth (authentication: AuthenticationParams): Promise<string> {
+  async auth (authentication: AuthenticationParams): Promise<AuthenticationModel> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
     if (account) {
       const isValid = await this.hashComparerStub.compare(authentication.password, account.password)
       if (isValid) {
         const accessToken = await this.encrypter.encrypt(account.id)
         await this.updatedAccessTokenRepository.updateAccessToken(account.id, accessToken)
-        return accessToken
+        return {
+          accessToken,
+          name: account.name
+        }
       }
     }
     return null
